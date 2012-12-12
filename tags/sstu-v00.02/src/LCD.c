2fa738e66fe0b514d7665136ed9025862a8e4090
vladislav.gyurov@gmail.com <vladislav.gyurov@gmail.com@36240e0c-f3fc-fde9-3c0d-4bb36602d2ab>
2012-12-12 20:39:03 +0000

    base revision (working copy)

---
+++ tags/sstu-v00.02/src/LCD.c
@@ -0,0 +1,347 @@
+/******************************************************************************/
+/* 
+ *                 			LCD INTERFACE FUNCTIONS             
+ *       
+*/
+/******************************************************************************/
+//     Note: rw permanently connected to gnd
+//     D2  rs
+//     D3  en
+//     D4  D4
+//     D5  D5
+//     D6  D6
+//     D7  D7 
+//     LCD pins D0-D3 are not used
+/*----------------------------------------------------------------------------*/
+/* Body Identification                                                        */
+/*----------------------------------------------------------------------------*/
+#ifdef LCD_C
+    #error "!!! FileName ID. It should be Unique !!!"
+#else
+    #define LCD_C
+#endif
+
+/*----------------------------------------------------------------------------*/
+/* Included files to resolve specific definitions in this file                */
+/*----------------------------------------------------------------------------*/
+#include "basedef.h"
+#include "LCD.h"
+#include "string.h"
+#include "stdarg.h"
+#include "stdio.h"
+#include "systmr.h"
+
+/*----------------------------------------------------------------------------*/
+/* Local constants                                                            */
+/*----------------------------------------------------------------------------*/ 
+#define LCD_TYPE			2u
+#define LCD_LINE_ADDRESS_1 	0x00u
+#define LCD_LINE_ADDRESS_2	0x40u
+
+/* Display ON/OFF Control defines */
+#define DON         0b00001111  /* Display on      */
+#define DOFF        0b00001011  /* Display off     */
+#define CURSOR_ON   0b00001111  /* Cursor on       */
+#define CURSOR_OFF  0b00001101  /* Cursor off      */
+#define BLINK_ON    0b00001111  /* Cursor Blink    */
+#define BLINK_OFF   0b00001110  /* Cursor No Blink */
+
+/* Cursor or Display Shift defines */
+#define SHIFT_CUR_LEFT    0b00010011  /* Cursor shifts to the left   */
+#define SHIFT_CUR_RIGHT   0b00010111  /* Cursor shifts to the right  */
+#define SHIFT_DISP_LEFT   0b00011011  /* Display shifts to the left  */
+#define SHIFT_DISP_RIGHT  0b00011111  /* Display shifts to the right */
+
+/* Function Set defines */
+#define FOUR_BIT   0b00101111  /* 4-bit Interface               */
+#define EIGHT_BIT  0b00111111  /* 8-bit Interface               */
+#define LINE_5X7   0b00110011  /* 5x7 characters, single line   */
+#define LINE_5X10  0b00110111  /* 5x10 characters               */
+#define LINES_5X7  0b00111011  /* 5x7 characters, multiple line */
+
+// Rev1 additional WH Tan
+// Addtional define to support display mode
+#define DISP_FLIP_NONE			0b00111100  /* No flip                             */
+#define DISP_FLIP_VERTICAL		0b00111101	/* Flip both vertically & horizontally */
+#define DISP_FLIP_HORIZONTAL	0b00111110	/* Flip horizontally                   */
+#define DISP_FLIP_BOTH			0b00111111	/* Flip vertically                     */
+// End Rev1
+
+// Rev1 additional WH Tan
+// Addtional define to support entry mode & shift mode
+#define ENTRY_CURSOR_DEC  		0b00000101	/* Entry cause cursor move to left  */
+#define ENTRY_CURSOR_INC		0b00000111	/* Entry cause cursor move to right */
+#define ENTRY_DISPLAY_SHIFT		0b00000111 	/* Entry cause the display to shift */
+#define ENTRY_DISPLAY_NO_SHIFT  0b00000110	/* Entry cuase no shift             */
+
+// Address defines
+// 4 lines X 20 characters LCD
+#ifdef LCD_4X20
+#define LINE0	0x00
+#define LINE1	0x40
+#define	LINE2	0x14
+#define LINE3	0x54
+#endif
+
+// 2 lines X 20 characters LCD
+#ifdef LCD_2X20
+#define LINE0	0x00
+#define LINE1	0x40
+#else
+
+// Use generic address
+#define LINE0	0x00
+#define LINE1	0x40
+#define	LINE2	0x14
+#define LINE3	0x54
+#endif
+
+/*----------------------------------------------------------------------------*/
+/* Local macros                                                               */
+/*----------------------------------------------------------------------------*/
+#define LCD_DATA_WRITE(DATA_NIBBLE)		pinLCD_DB4 = DATA_NIBBLE;		\
+										pinLCD_DB5 = DATA_NIBBLE >> 1;	\
+										pinLCD_DB6 = DATA_NIBBLE >> 2;	\
+										pinLCD_DB7 = DATA_NIBBLE >> 3;		
+
+/*----------------------------------------------------------------------------*/
+/* Local types                                                                */
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/* Local data                                                                 */
+/*----------------------------------------------------------------------------*/
+const U8 LCD_INIT_STRING[4] = 
+	{
+	0x20 | (LCD_TYPE << 2), // Func set: 4-bit, 2 lines, 5x8 dots 
+	0xc,					// Display on 			
+ 	1,						// Clear display 
+ 	6						// Increment cursor 
+ 	};
+
+U16 u16Nibble_Temp;
+
+/*----------------------------------------------------------------------------*/
+/* Constant local data                                                        */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Exported data                                                              */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Constant exported data                                                     */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Local function prototypes                                                  */
+/*----------------------------------------------------------------------------*/
+void LCDSendNibble(U8 u8Nibble);
+void LCDSendByte(U8 u8ByteToLcd,U8 u8Adress);
+
+/******************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/*                   I N T E R F A C E   F U N C T I O N S                    */
+/*                                                                            */
+/******************************************************************************/
+/******************************************************************************/
+/*
+ * Name:    IF_LCDInit 
+ * Purpose: init LCD
+ * Input: none
+ * Output:none
+*/
+/******************************************************************************/
+void IF_LCDInit(void)
+{
+	U16 u16InitCounter;
+	
+	pinLCD_RS = 0;
+	pinLCD_E =0;
+	delay_ms(15);
+	
+	for(u16InitCounter=1;u16InitCounter<=3;++u16InitCounter)
+	{
+		LCDSendNibble(3);
+		delay_ms(5);
+	}//end for
+	
+	LCDSendNibble(2);
+	
+	for(u16InitCounter=0;u16InitCounter<=3;++u16InitCounter)
+	{
+		LCDSendByte(LCD_INIT_STRING[u16InitCounter],0);	
+	}//end for
+}
+/******************************************************************************/
+/*
+ * Name:    IF_LCDPutc
+ * Purpose: put character to lcd
+ * Input: U8 u8CharToLcd
+ * Output:none
+ *  
+ * The following have special meaning:  
+ * \f  Clear display                   
+ * \n  Go to start of second line  
+ * \b  Move back one position  
+ */
+/******************************************************************************/
+void IF_LCDPutc(U8 u8CharToLcd)
+{
+	switch(u8CharToLcd)
+	{
+	case '\f': 	LCDSendByte(1,0);
+				delay_us(120);
+				break;
+	
+	case '\n': 	IF_LCDGotoXY(1,2);
+				break;	
+	case '\b': 	LCDSendByte(0x10,0);
+				break;		
+	default:	LCDSendByte(u8CharToLcd,1);
+
+	}
+
+}
+/******************************************************************************/
+/*
+ * Name:    IF_LCDGotoXY()
+ * Purpose: goto xy lcd.Set write position on LCD (upper left is 1,1)  
+ * Input: U8 x,U8 y
+ * Output:none
+*/
+/******************************************************************************/
+void IF_LCDGotoXY(U8 x,U8 y)
+{
+	U8 u8Addr;
+	
+	if(y!=1)
+	{
+		u8Addr = LCD_LINE_ADDRESS_2;
+	}//end if
+	else
+	u8Addr = 0;
+	
+	u8Addr+=x-1;
+	LCDSendByte(0x80|u8Addr,0);
+}
+/******************************************************************************/
+/*
+ * Name:    IF_LCDPuts
+ * Purpose: put string to lcd
+ * Input: U8 *u8StrToLcd
+ * Output:none
+*/
+/******************************************************************************/
+void IF_LCDPuts(const char *u8StrToLcd)
+{
+	//U16 u16CharCount;
+	
+	//u16CharCount = strlen(u8StrToLcd);
+	while(*u8StrToLcd)
+	{
+		LCDSendByte(*u8StrToLcd,1);
+		u8StrToLcd++;
+	//u16CharCount--;
+	}
+
+}
+/******************************************************************************/
+/*
+ * Name:  
+ * Purpose: Display unsigned number
+ * Input: Unsigned int
+ * Output: none
+*/
+/******************************************************************************/
+void IF_LCDPutn(U32 t)
+{
+	unsigned char t1,i,wrote;
+	U32 k;
+	U32 c;
+
+    c = t;
+	wrote = 0;
+	for (i=4;i>=1;i--)
+	{
+		switch(i)
+		{
+			case 4: k=10000;
+				break;
+			case 3: k=1000;
+				break;
+			case 2: k=100;
+				break;
+			case 1: k=10;
+		}
+		t1 = c / k;
+		if((wrote) || (t1!=0))
+		{
+			IF_LCDPutc(t1+'0');
+			wrote=  1;
+		}
+		c -= (t1 * k);
+	}
+	
+	IF_LCDPutc(c+'0');
+}
+/******************************************************************************/
+/*
+ * Purpose: Display Current System state in upper right display position
+ * Inputs:  none
+ * Output:  none
+ */
+/******************************************************************************/
+void IF_LCD_UpdateState(U16 u16CurrentState)
+{
+	
+	IF_LCDGotoXY(15,1);
+	IF_LCDPutn(u16CurrentState);
+
+}
+
+/******************************************************************************/
+/******************************************************************************/
+/*                                                                            */
+/*                       L O C A L   F U N C T I O N S                        */
+/*                                                                            */
+/******************************************************************************/
+/******************************************************************************/
+/*
+ * Name: LCDSedndNibble  
+ * Purpose: Send Nibble to lcd
+ * Input: input param is byte
+ * Output:none
+*/
+/******************************************************************************/
+void LCDSendNibble(U8 u8Nibble)
+{
+	LCD_DATA_WRITE(u8Nibble);
+	Nop();
+	Nop();
+	pinLCD_E = 1;
+	delay_us(5);
+	Nop();
+	pinLCD_E = 0;
+
+}
+/******************************************************************************/
+/*
+ * Name:    LCDSedndByte 
+ * Purpose: Send byte to lcd
+ * Input: input param is byte
+ * Output:none
+*/
+/******************************************************************************/
+void LCDSendByte(U8 u8ByteToLcd,U8 u8Adress)
+{
+	pinLCD_RS = 0;
+	delay_us(60);
+	pinLCD_RS = u8Adress;
+	Nop();
+	Nop();
+	pinLCD_E = 0;
+	LCDSendNibble(u8ByteToLcd >> 4);
+	LCDSendNibble(u8ByteToLcd & 0xF);
+}
+
---
+++ tags/sstu-v00.02/src/include/statemachine.h
@@ -0,0 +1,252 @@
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+    
+/*----------------------------------------------------------------------------*/
+/* Included files to resolve specific definitions in this file                */
+/*----------------------------------------------------------------------------*/
+#include "basedef.h"
+
+/*----------------------------------------------------------------------------*/
+/* Constant data                                                              */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Exported type                                                              */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Exported data                                                              */
+/*----------------------------------------------------------------------------*/
+extern volatile U16 SYS_CONTROL;
+extern volatile U16 PC_CONTROL;
+extern volatile U16 ERR_CONTROL;
+
+/*----------------------------------------------------------------------------*/
+/* Constant exported data                                                     */
+/*----------------------------------------------------------------------------*/
+
+/* ST1 STATES - Statemachine in task1. Control management. */
+#define ST1_INIT					20
+#define ST1_RESET_ERRORS			21
+#define ST1_CHECK_ERRORS			22
+#define ST1_DC_AUTOTUNNING			23
+#define ST1_IDENT_FIRST				24
+#define ST1_IDENT					25
+#define ST1_TMPCTRL					26
+#define ST1_STAND					27
+#define ST1_EXTRACTOR				28
+#define ST1_NO_CARTRIDGE			29
+#define ST1_HIBERNATE				30
+//Free							  //31	
+#define ST1_ERROR_STATE				32
+//Free							  //33
+#define ST1_WAIT_STATE				34
+#define ST1_TEST_MODE_IDENT			35
+#define ST1_LEARNING_MODE			36
+
+/* ST2 STATES - Statemachine in task2. User management. */
+#define ST2_INIT					100
+#define ST2_WAIT_POWER_UP			101
+#define ST2_POWER_UP_OK				102
+#define ST2_CHECKING_CARTRIDGE		103
+#define ST2_TMPCTRL					104
+#define ST2_STAND					105
+#define ST2_EXTRACTOR				106
+#define ST2_NO_CARTRIDGE			107
+#define ST2_HIBERNATE				108
+#define ST2_CHANGE_USER_REF			109
+#define ST2_TEMP_UP					110
+#define ST2_TEMP_DOWN				111
+#define ST2_TEMP_SELECT				112
+#define ST2_ERROR_STATE				113
+#define ST2_WAIT_STATE				114
+#define ST2_WAIT_STATE_II			115
+
+
+/*----------------------------------------------------------------------------*/
+/* Exported Macros                                                            */
+/*----------------------------------------------------------------------------*/
+#define _power_up_ok()					(_getbit(SYS_CONTROL,0))  	 /* bit 0 */
+#define _set_power_up(x)				(_putbit((x),SYS_CONTROL,0)) /* bit 0 */
+
+#define _drive_enabled()				(_getbit(SYS_CONTROL,1))  	 /* bit 1 */
+#define _set_drive(x)					(_putbit((x),SYS_CONTROL,1)) /* bit 1 */
+
+#define _select_button_pushed()			(_getbit(SYS_CONTROL,2))  	 /* bit 2 */
+#define _set_select_button(x)			(_putbit((x),SYS_CONTROL,2)) /* bit 2 */
+
+#define _up_button_pushed()				(_getbit(SYS_CONTROL,3))  	 /* bit 3 */
+#define _set_up_button(x)				(_putbit((x),SYS_CONTROL,3)) /* bit 3 */
+
+#define _down_button_pushed()			(_getbit(SYS_CONTROL,4))  	 /* bit 4 */
+#define _set_down_button(x)				(_putbit((x),SYS_CONTROL,4)) /* bit 4 */
+
+#define _extractor()					(_getbit(SYS_CONTROL,5))  	 /* bit 5 */
+#define _set_extractor(x)				(_putbit((x),SYS_CONTROL,5)) /* bit 5 */
+
+#define _stand()						(_getbit(SYS_CONTROL,6))  	 /* bit 6 */
+#define _set_stand(x)					(_putbit((x),SYS_CONTROL,6)) /* bit 6 */
+
+#define _cartridge_present()			(_getbit(SYS_CONTROL,7))  	 /* bit 7 */
+#define _set_cartridge_present(x)		(_putbit((x),SYS_CONTROL,7)) /* bit 7 */
+
+#define _zero_cross()					(_getbit(SYS_CONTROL,8))  	 /* bit 8 */
+#define _set_zero_cross(x)				(_putbit((x),SYS_CONTROL,8)) /* bit 8 */
+
+#define _error_check()					(_getbit(SYS_CONTROL,9))  	 /* bit 9 */
+#define _set_error_check(x)				(_putbit((x),SYS_CONTROL,9)) /* bit 9 */
+
+#define _hibernate()					(_getbit(SYS_CONTROL,10))  	 /* bit 10 */
+#define _set_hibernate(x)				(_putbit((x),SYS_CONTROL,10)) /* bit 10 */
+
+#define _eeprom_busy()					(_getbit(SYS_CONTROL,11))  	 /* bit 11 */
+#define _set_eeprom_busy(x)				(_putbit((x),SYS_CONTROL,11)) /* bit 11 */
+
+#define _triac_state()					(_getbit(SYS_CONTROL,12))  	 /* bit 12 */
+#define _set_triac_state(x)				(_putbit((x),SYS_CONTROL,12)) /* bit 12 */
+
+#define _over_prot_triac_state()		(_getbit(SYS_CONTROL,13))  	 /* bit 13 */
+#define _set_over_prot_triac_state(x)	(_putbit((x),SYS_CONTROL,13)) /* bit 13 */
+
+//#define _()							(_getbit(SYS_CONTROL,14))  	 /* bit 14 */
+//#define _(x)							(_putbit((x),SYS_CONTROL,14)) /* bit 14 */
+
+//#define _()							(_getbit(SYS_CONTROL,15))  	 /* bit 15 */
+//#define _(x)							(_putbit((x),SYS_CONTROL,15)) /* bit 15 */
+
+
+/* SYSTEM CONTROL  */
+/*	
+	SYS_CONTROL 
+	Bit 0		-	Power UP OK 			1: Power Up OK 0: Power Up Fail	
+	Bit 1		-	Drive Enable			1: Drive ENABLED  0: Drive DISABLED	
+	Bit 2		-	pinBUT_SEL  			1: SELECT button pushed  0: SELECT button NOT pushed. Note that value is inverted from IO PORT!!!
+	Bit 3		-	pinBUT_UP  				1: UP button pushed  0: UP button NOT pushed. Note that value is inverted from IO PORT!!!	
+	Bit 4		-	pinBUT_DOWN				1: DOWN button pushed  0: DOWN button NOT pushed. Note that value is inverted from IO PORT!!!
+	Bit 5		-	pinEXTR					1: Cartridge is in EXTRACTOR  0: Cartridge is NOT in EXTRACTOR	
+	Bit 6		-	pinSTAND				1: Cartridge is in STAND  0: Cartridge is NOT in STAND
+	Bit 7		-	Cart. Present			1: Cartridge is present in system  0: Cartridge is not present
+	Bit 8		-	ZERO CROSS				1: Zero cross occur 0: No zero cross. Note: Zero cross is "live" one task1 period /100us/.
+	Bit 9		-	ERR_CHECK				1: Check foe errors in beginning of ST1 state machine  0: Do not check for errors in beginning of ST1 state machine
+	Bit 10		-	Hibernate				1: Hibernation	0: Norm modes
+	Bit 11		-	EE Busy					1: EEprom busy (operation performed)	0: EEprom NOT busy
+	Bit 12		-	Triac state				1: Triac fired 0: Triac not fired 	Note: bit is set from macros in triac_control.h
+	Bit 13		-	OVERPROT triac state	1: Triac Fired	0: Triac not fired. set in macros in triac_control.h Note: Cleared in overload protection routine.
+	Bit 14		-	
+	Bit 15		-	
+*/
+
+
+/* PC CONTROL FLAGS */
+//#define _()							(_getbit(PC_CONTROL,0))     /* bit 0 */
+//#define _(x)							(_putbit((x),PC_CONTROL,0)) /* bit 0 */
+
+//#define _()							(_getbit(PC_CONTROL,1))     /* bit 1 */
+//#define _(x)							(_putbit((x),PC_CONTROL,1)) /* bit 1 */
+
+//#define _()							(_getbit(PC_CONTROL,2))     /* bit 2 */
+//#define _(x)							(_putbit((x),PC_CONTROL,2)) /* bit 2 */
+
+#define _reset_error_reg()				(_getbit(PC_CONTROL,3))     /* bit 3 */
+#define _set_reset_error_reg(x)			(_putbit((x),PC_CONTROL,3)) /* bit 3 */
+
+//#define _()							(_getbit(PC_CONTROL,4))     /* bit 4 */
+//#define _(x)							(_putbit((x),PC_CONTROL,4)) /* bit 4 */
+
+
+/*	
+	PC_CONTROL 
+	Bit 0		-	
+	Bit 1		-	
+	Bit 2		-	
+	Bit 3		-	Reset error register ERR_CONTROL 	1: Reset  0: Do not reset
+	Bit 4		-	
+	Bit 5		-	
+	Bit 6		-	
+	Bit 7		-	
+	Bit 8		-
+	Bit 9		-
+	Bit 10		-
+	Bit 11		-
+	Bit 12		-
+	Bit 13		-
+	Bit 14		-	
+	Bit 15		-	
+*/
+
+/* ERROR CONTROL MACROS */
+#define _RESET_ERROR_INTERNAL()			ERR_CONTROL = 0
+
+/* Reset error register macro */
+#define _RESET_ERROR_PC_HANDLER()(	\
+{									\
+	if(_reset_error_reg())			\
+	{								\
+	ERR_CONTROL = 0;				\
+	_set_reset_error_reg(0);		\
+	}								\
+})
+									
+#define _grid_freq_error()				(_getbit(ERR_CONTROL,0))  	 /* bit 0 */
+#define _set_grid_freq_error(x)			(_putbit((x),ERR_CONTROL,0)) /* bit 0 */
+
+#define _ee_checksum_read_error()		(_getbit(ERR_CONTROL,1))  	 /* bit 1 */
+#define _set_ee_checksum_read_error(x)	(_putbit((x),ERR_CONTROL,1)) /* bit 1 */
+
+#define _param_limit_error()			(_getbit(ERR_CONTROL,2))  	 /* bit 2 */
+#define _set_param_limit_error(x)		(_putbit((x),ERR_CONTROL,2)) /* bit 2 */
+
+#define _overtemperature_error()		(_getbit(ERR_CONTROL,3))  	 /* bit 3 */
+#define _set_overtemperature_error(x)	(_putbit((x),ERR_CONTROL,3)) /* bit 3 */
+
+#define _ident_error()					(_getbit(ERR_CONTROL,4))  	 /* bit 4 */
+#define _set_ident_error(x)				(_putbit((x),ERR_CONTROL,4)) /* bit 4 */
+
+#define _offset_current_error()			(_getbit(ERR_CONTROL,5))  	 /* bit 5 */
+#define _set_current_offset_error(x)	(_putbit((x),ERR_CONTROL,5)) /* bit 5 */
+
+#define _overcurrent_error()			(_getbit(ERR_CONTROL,6))  	 /* bit 6 */
+#define _set_overcurrent_error(x)		(_putbit((x),ERR_CONTROL,6)) /* bit 6 */
+
+//#define _()							(_getbit(ERR_CONTROL,7))  	 /* bit 7 */
+//#define _(x)							(_putbit((x),ERR_CONTROL,7)) /* bit 7 */
+
+#define _ee_size_error()                (_getbit(ERR_CONTROL,8))  	 /* bit 8 */
+#define _set_ee_size_error(x)           (_putbit((x),ERR_CONTROL,8)) /* bit 8 */
+
+#define _ee_write_error()				(_getbit(ERR_CONTROL,9))  	 /* bit 9 */
+#define _set_ee_write_error(x)			(_putbit((x),ERR_CONTROL,9)) /* bit 9 */
+
+#define _overload_error()				(_getbit(ERR_CONTROL,10))  	  /* bit 10 */
+#define _set_overload_error(x)			(_putbit((x),ERR_CONTROL,10)) /* bit 10 */
+
+#define _ee_valid_error()				(_getbit(ERR_CONTROL,11))  	  /* bit 11 */
+#define _set_ee_valid_error(x)			(_putbit((x),ERR_CONTROL,11)) /* bit 11 */
+
+/*	
+	ERR_CONTROL 
+	Bit 0		-	Grid Frequency_error			1: Line overfrequency  0: Line freq OK
+	Bit 1		-	Checksum Error EEPROM Read		1: Checksum Error read	0: OK
+	Bit 2		-	Prameters error					1: Param errors detected in -> void Params_check_limit(void)	0: OK
+	Bit 3		-	Overtemperature trip  			1: Overtemperature  0: Temperature OK
+	Bit 4		-	IDENT ERROR 					1: IDENT ERROR  0: IDENT OK		
+	Bit 5		-	OFFSET CURRENT ERROR			1: Offset error in current mesasure channel  
+	Bit 6		-	OVERCURRENT ERROR				1: Overcurrent error 0: Current OK
+	Bit 7		-	
+	Bit 8		-		
+	Bit 9		-	EE SIZE ERROR                   1: EEPROM size fault        0: OK
+	Bit 10		-	OVERLOAD ERROR					1: Cartridge overload. Too many heat periods over cartridge.		0: OK
+	Bit 11		- 	EE_VALID						1: Invalid EEPROM Params	0: OK
+	Bit 12		-   
+	Bit 13		-   
+	Bit 14		-
+*/
+
+/*----------------------------------------------------------------------------*/
+/* Exported functions          					           				      */
+/*----------------------------------------------------------------------------*/
+void state_machine_T1(void);
+void state_machine_T2(void);
+void state_machine_init(void);
+
+#endif /* STATEMACHINE_H */
